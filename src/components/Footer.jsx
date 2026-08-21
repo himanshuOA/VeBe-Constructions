637ec947b699484389b017112f1c901c7b9be507
himanshuOA <himanshu@ocularityanalytics.com>
2026-08-21 13:12:29 +0530

    add email2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,8 +32,8 @@ export default function Footer() {
         <div>
           <h4>Contact</h4>
           <ul>
-            <li><a href="mailto:vebeinteriors2k22@gmail.com">vebeinteriors2k22@gmail.com</a></li>
             <li><a href="mailto:info@vebeinteriors.com">info@vebeinteriors.com</a></li>
+            <li><a href="tel:+919311217408">+91 93112 17408</a></li>
             <li><span>Residential &amp; Commercial</span></li>
           </ul>
         </div>
